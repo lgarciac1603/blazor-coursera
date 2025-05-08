620eb263cd0c8eba1f1a027de3a4f09aa85c44b9
lgarciac1603 <lfgc851@gmail.com>
2025-05-07 20:00:30 -0600

    Blazor final proyect

---
+++ EventEase/Models/Event.cs
@@ -0,0 +1,9 @@
+namespace EventEase.Models
+{
+  public class Event
+  {
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public string Location { get; set; }
+  }
+}
